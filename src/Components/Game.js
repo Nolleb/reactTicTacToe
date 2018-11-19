3fff0cf4df0aadd340a28a5178f3ab19c166746c
Roger Bellon <rbellongronnier@gmail.com>
2018-11-19 21:40:40 +0100

    cleaning

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -18,18 +18,22 @@ class Game extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
+
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+        
         this.setState({
             history: history.concat([{
-            squares: squares,
+            squares: squares
         }]),
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext
         });
     }
+
     jumpTo(step) {
         this.setState({
             stepNumber: step,
@@ -46,23 +50,26 @@ class Game extends React.Component {
             'Go to move #' + move :
             'Go to game start';
             return (
-            <li key={move}>
-                <button onClick={() => this.jumpTo(move)}>{desc}</button>
-            </li>
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
             );
         });
+
         let status;
+
         if (winner) {
             status = 'Winner: ' + winner;
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
+
         return(
             <div className="game">
                 <div className="game-board">
                     <Board 
-                    squares={current.squares}
-                    onClick={(i) => this.handleClick(i)}
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
                     />
                 </div>
                 <div className="game-info">
